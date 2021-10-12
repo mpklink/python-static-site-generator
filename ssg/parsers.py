aff7f35c02903b54be9bbf96f55e1d68803e9eab
Mike Klink <michael.klink@icumed.com>
2021-10-12 09:32:20 -0500

    fixed read and write

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -10,4 +10,11 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
-    def read(self, path)
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
